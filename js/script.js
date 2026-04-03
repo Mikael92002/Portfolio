7dc8559b62cb8734e81e2b10d45d8c1373386455
Mikael92002 <Mikael92002@users.noreply.github.com>
2026-04-03 04:03:48 -0500

    Updated README

---
+++ js/script.js
@@ -0,0 +1,26 @@
+document.addEventListener("DOMContentLoaded", (event) => {
+  const firstName = ".first_name>div";
+  const lastName = ".last_name>div";
+  const intro = ".intro>div";
+  const heroImg = ".hero_img_container>img";
+  gsap.set([firstName, lastName, intro, heroImg], { visibility: "visible" });
+
+  const tl = gsap.timeline();
+  // the -=0.8 provides overlap, so 2nd anim for e.g. plays
+  // 0.2s after 1st anim
+  // < means start right after previous anim started
+  // > means start right after previous anim ended (default behavior)
+  
+  tl.from(firstName, { xPercent: -125, duration: 1, ease: "circ.out" })
+    .from(lastName, { yPercent: 100, duration: 1, ease: "circ.out" }, "-=0.8")
+    .from(intro, { yPercent: -100, duration: 1 }, "-=0.8")
+    .from(
+      heroImg,
+      {
+        xPercent: 100,
+        opacity: 0,
+        duration: 1,
+      },
+      "-=0.6",
+    );
+});
