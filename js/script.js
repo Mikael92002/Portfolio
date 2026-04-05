ed803dd3e9434d2425a99760b504f961d381c80d
Mikael92002 <Mikael92002@users.noreply.github.com>
2026-04-04 21:40:49 -0500

    Added pill animation

--- js/script.js
+++ js/script.js
@@ -4,8 +4,12 @@ document.addEventListener("DOMContentLoaded", (event) => {
   const name = ".name>div";
   const heroImg = ".hero_img_container>img";
   const jobTitle = ".job-title>div";
-  const facts = ".alt-titles>div";
-  gsap.set([name, heroImg, jobTitle, facts], { visibility: "visible" });
+  const altTitles = ".alt-titles>div";
+  const location = ".location>div";
+  const pill = ".pill";
+  gsap.set([name, heroImg, jobTitle, altTitles, location, pill], {
+    visibility: "visible",
+  });
 
   const tl = gsap.timeline();
   // the -=0.8 provides overlap, so 2nd anim for e.g. plays
@@ -28,6 +32,26 @@ document.addEventListener("DOMContentLoaded", (event) => {
       },
       "<",
     )
-    .from(facts, { xPercent: -100, opacity: 0, duration: 1 }, "<")
+    .from(altTitles, { xPercent: -100, opacity: 0, duration: 1 }, "<")
+    .from(location, { xPercent: 100, opacity: 0, duration: 1 }, "<")
+    .from(pill, { opacity: 0, duration: 1 }, "<")
     .from(jobTitle, { opacity: 0, duration: 0.5 }, ">");
+
+  // pill yoyo:
+  const pillTl = gsap.timeline({
+    repeat: -1,
+    delay: 1,
+  });
+
+  const loopPill = gsap.timeline({ repeat: 3 });
+
+  loopPill
+    .to(pill, { rotation: 20, ease: "power1.inOut", duration: 0.2 })
+    .to(pill, { rotation: -20, ease: "power1.inOut", duration: 0.2 });
+
+  pillTl
+    .to(pill, { scale: 1.2, duration: 0.5, ease: "power3.inOut" })
+    .add(loopPill, "-=0.5")
+    .to(pill, { scale: 1, duration: 0.5, ease: "power3.inOut" }, "-=0.5")
+    .to(pill, { duration: 4 });
 });
