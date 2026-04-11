769dda62b765eb0fbf512baed086e2d4786f4ee5
Mikael92002 <Mikael92002@users.noreply.github.com>
2026-04-10 21:49:22 -0500

    Working on plane animation

--- js/script.js
+++ js/script.js
@@ -145,20 +145,30 @@ function resizeInvisibleText() {
 window.addEventListener("load", resizeInvisibleText);
 window.addEventListener("resize", resizeInvisibleText);
 
-
 const planeImg = document.querySelector(".plane>img");
-console.log(planeImg.offsetWidth)
-gsap.to(".plane>img", {
+console.log(planeImg.offsetWidth);
+
+const contactSection = "#contact";
+
+const contactSectionTl = gsap.timeline({
+  scrollTrigger: {
+    trigger: contactSection,
+    pin: true,
+    pinSpacing: false,
+    start: "top bottom",
+    end: "bottom top-=60px",
+    scrub: true,
+    invalidateOnRefresh: true,
+    markers: true,
+  },
+});
+
+contactSectionTl.to(".plane>img", {
   motionPath: {
-    path: [
-      // { x: 100, y: 50 },
-      // { x: 200, y: 0 },
-      { x: window.innerWidth-planeImg.offsetWidth, y: 100 },
-    ],
+    path: [{ x: window.innerWidth, y: 150 }],
     alignOrigin: [0.5, 0.5],
     autoRotate: true,
   },
   transformOrigin: "50% 50%",
-  duration: 5,
   ease: "power1.inOut",
 });
